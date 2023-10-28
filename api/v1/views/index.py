3dc1b86321f9aadd12f44d51a5979f46a65439a4
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-28 06:51:58 -0700

    counting objects types

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,28 @@
 """returns a json string"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """status of API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat_counts():
+    """ retrieves the number of each objects"""
+    stats = {}
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+    for i in range(len(classes)):
+        stats[names[i]] = storage.count(classes[i])
+
+    return jsonify(stats)
