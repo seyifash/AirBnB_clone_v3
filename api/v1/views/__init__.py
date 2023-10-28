c93d431f7516357db154eb5543753e265e0ae015
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-28 08:54:14 -0700

    the fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,8 +5,5 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
-<<<<<<< HEAD
 from api.v1.views.cities import *
-=======
 from api.v1.views.states import *
->>>>>>> 59a334b98bfae0c99fa6c82defa19005825cd87f
