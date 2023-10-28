b20891d32d56b815dd39981149d7f71964129381
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-28 13:19:45 -0700

    cross origin

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,13 @@
 """ creating an api"""
 from flask import Flask, make_response, jsonify
 from models import storage
+from flask_cors import CORS
 import os
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
