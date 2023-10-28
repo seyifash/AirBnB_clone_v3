e7e46217dd18c760fbde3bcc2fa3a8d51f579ba7
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-28 09:12:37 -0700

    adding the delete method

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -27,6 +27,17 @@ def get_city(city_id):
     return jsonify(city.to_dict())
 
 
+@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
+def delete_city(city_id):
+    """Deletes a City object by city_id"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    storage.delete(city)
+    storage.save()
+    return jsonify({})
+
+
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
                  strict_slashes=False)
 def post_city(state_id):
@@ -48,12 +59,12 @@ def post_city(state_id):
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def put_city(city_id):
     """Updates a City object by city_id"""
-    data = request.get_json()
-    if data is None:
-        abort(400, description="Not a JSON")
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, description="Not a JSON")
     for key, value in data.items():
         if key not in ['id', 'state_id', 'created_at', 'updated_at']:
             setattr(city, key, value)
